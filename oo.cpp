89e6b236a651c7c0df2fbc698a8414f9afcd8d3f
hollacs <vip-colgate@hotmail.com>
2024-07-17 03:08:49 +0800

    init amxx module

---
+++ oo.cpp
@@ -0,0 +1,15 @@
+#include "amxxmodule.h"
+
+void OnAmxxAttach()
+{
+}
+
+void OnPluginsLoaded()
+{
+    MF_PrintSrvConsole("OO is loaded\n");
+}
+
+void OnPluginsUnloaded()
+{
+    MF_PrintSrvConsole("OO is unloaded\n");
+}
